c1e69ce47683bfc47a47675c7fa4b753b9eb9821
acaporicci <antoncourtois@gmail.com>
2023-01-30 20:08:00 -0500

    Commit a new version

--- run_analysis.R
+++ run_analysis.R
@@ -12,6 +12,7 @@ if (!file.exists("UCI HAR Dataset")) {
   unzip(filename) 
 }
 
+# Create the dataframes
 features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
@@ -21,15 +22,19 @@ subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
+#Merge the two sets
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
 Merged_Data <- cbind(Subject, Y, X)
 
+#Subset the data to get only the measurements for mean and standard deviation
 TempData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
 
+#Rename activities to improve readability
 TempData$code <- activities[TempData$code, 2]
 
+#Rename variable names to improve readability
 names(TempData)[2] = "activity"
 names(TempData)<-gsub("Acc", "Accelerometer", names(TempData))
 names(TempData)<-gsub("Gyro", "Gyroscope", names(TempData))
@@ -44,6 +49,7 @@ names(TempData)<-gsub("-freq()", "Frequency", names(TempData), ignore.case = TRU
 names(TempData)<-gsub("angle", "Angle", names(TempData))
 names(TempData)<-gsub("gravity", "Gravity", names(TempData))
 
+#Pipe the final data in a text file
 FinalData <- TempData %>%
   group_by(subject, activity) %>%
   summarise_all(funs(mean))
